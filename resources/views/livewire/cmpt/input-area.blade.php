da6b423de20a90058300fd25d5f204630d243984
Duy Anh Dinh <duyanhscor@gmail.com>
2024-01-31 11:14:11 +0700

    add debounce

--- resources/views/livewire/cmpt/input-area.blade.php
+++ resources/views/livewire/cmpt/input-area.blade.php
@@ -2,7 +2,7 @@
     <textarea
         class="absolute w-full resize-none rounded-md border border-gray-400 h-full text-[18px] pb-[40px] whitespace-pre-wrap"
         x-model="$wire.input"
-        @keyup="$wire.validCharacter()"
+        @keyup.debounce="$wire.validCharacter()"
     ></textarea>
 
     <div class="absolute bottom-0 w-full h-[40px] bg-white pr-[20px] rounded-b-md border border-gray-400 border-t-0 flex items-center">
